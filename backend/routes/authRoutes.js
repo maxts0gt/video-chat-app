a5ca81c1b630b2f88e9f1510dc97dce95794d80d
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-22 21:36:58 +0900

    adding required

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,14 +4,17 @@ const authControllers = require("../controllers/auth/authControllers");
 const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
 
+// validating for register
 const registerSchema = Joi.object({
-  email: Joi.string().email(),
-  username: Joi.string().min(3).max(12),
-  password: Joi.string().min(6).max(12),
+  email: Joi.string().email().required(),
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
 });
+
+// validating for login
 const loginSchema = Joi.object({
-  email: Joi.string().email(),
-  password: Joi.string().min(6).max(12),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).max(12).required(),
 });
 
 router.post(
