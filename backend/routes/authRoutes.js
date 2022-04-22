22ceebd103fa797e8d18f8f8bcc4eee740253a74
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-22 21:26:26 +0900

    implement validator for login and register schema

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const authControllers = require("../controllers/auth/authControllers");
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
 
-router.post("/register", authControllers.controllers.postRegister);
-router.post("/login", authControllers.controllers.postLogin);
+const registerSchema = Joi.object({
+  email: Joi.string().email(),
+  username: Joi.string().min(3).max(12),
+  password: Joi.string().min(6).max(12),
+});
+const loginSchema = Joi.object({
+  email: Joi.string().email(),
+  password: Joi.string().min(6).max(12),
+});
+
+router.post(
+  "/register",
+  validator.body(registerSchema),
+  authControllers.controllers.postRegister
+);
+router.post(
+  "/login",
+  validator.body(loginSchema),
+  authControllers.controllers.postLogin
+);
 
 module.exports = router;
