679380c5e6429a3c7a4fce2976b33020275b9aef
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-23 23:16:04 +0900

    create tooltip for login button

--- frontend/src/authPages/LoginPage/LoginPageFooter.js
+++ frontend/src/authPages/LoginPage/LoginPageFooter.js
@@ -2,6 +2,17 @@ import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
 import RedirectInfo from "../../shared/components/RedirectInfo";
 import { useNavigate } from "react-router-dom";
+import { Tooltip } from "@mui/material";
+
+const getFormNotValidMessage = () => {
+  //
+  return "Enter correct e-mail address and password should contain 6 to 12 characters";
+};
+
+const getFormValidMessage = () => {
+  //
+  return "Press to log in!";
+};
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
   const navigate = useNavigate();
@@ -12,15 +23,18 @@ const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 
   return (
     <>
-      <div>
-        <CustomPrimaryButton
-          label="Log In"
-          additionalStyles={{ marginTop: "30px", cursor: "pointer" }}
-          disabled={!isFormValid}
-          onClick={handleLogin}
-        />
-      </div>
-
+      <Tooltip
+        title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}
+      >
+        <div>
+          <CustomPrimaryButton
+            label="Log In"
+            additionalStyles={{ marginTop: "30px", cursor: "pointer" }}
+            disabled={!isFormValid}
+            onClick={handleLogin}
+          />
+        </div>
+      </Tooltip>
       <RedirectInfo
         text="Don't have an account?"
         redirectText="Create an account"
