d19cb275144f14f6a5336bde3428f6850265c5d5
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-23 23:35:11 +0900

    handle validators

--- frontend/src/authPages/LoginPage/LoginPage.js
+++ frontend/src/authPages/LoginPage/LoginPage.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AuthBox from "../../shared/components/AuthBox";
 import LoginPageFooter from "./LoginPageFooter";
 import LoginPageHeader from "./LoginPageHeader";
 import LoginPageInputs from "./LoginPageInputs";
+import { validateLoginForm } from "../../shared/utils/validators";
 
 function LoginPage() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
 
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+
   const handleLogin = () => {
+    console.log(mail);
+    console.log(password);
     console.log("Login in");
   };
 
