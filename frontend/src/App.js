15dbc9f254785c29bb9041db3cc0529068f794ef
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-23 20:09:02 +0900

    setup navigation with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+import Dashboard from "./Dashboard/Dashboard";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
