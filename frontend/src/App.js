6a8ae2c859cb2aa0422cf7cb6848ee597968e8bb
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-26 20:34:54 +0900

    create alert for notification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React from 'react';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import "./App.css";
-import LoginPage from "./authPages/LoginPage/LoginPage";
-import RegisterPage from "./authPages/RegisterPage/RegisterPage";
-import Dashboard from "./Dashboard/Dashboard";
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+import './App.css';
+import LoginPage from './authPages/LoginPage/LoginPage';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
+import Dashboard from './Dashboard/Dashboard';
+import AlertNotification from './shared/components/AlertNotification';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/register" element={<RegisterPage />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route exact path='/login' element={<LoginPage />} />
+					<Route exact path='/register' element={<RegisterPage />} />
+					<Route exact path='/dashboard' element={<Dashboard />} />
+					<Route
+						path='/'
+						element={<Navigate to='/dashboard' replace />}
+					/>
+				</Routes>
+			</Router>
+			<AlertNotification />
+		</>
+	);
 }
 
 export default App;
---
+++ frontend/src/shared/components/AlertNotification.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+import { connect } from 'react-redux';
+
+const AlertNotification = (props) => {
+	return (
+		<Snackbar
+			anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+			open
+			onClose={() => {}}
+			// autoHideDuration={6000}
+		>
+			<Alert severity='info'>Alert Message</Alert>
+			{/*  */}
+		</Snackbar>
+	);
+};
+
+export default AlertNotification;
