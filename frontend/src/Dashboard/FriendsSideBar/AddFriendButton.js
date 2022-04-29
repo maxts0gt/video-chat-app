7315267d892eddcbf69b516c40018dcbc430e214
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-29 20:51:54 +0900

    add friend button

---
+++ frontend/src/Dashboard/FriendsSideBar/AddFriendButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CustomFriendButton from '../../shared/components/CustomPrimaryButton';
+
+const additionalStyles = {
+	marginTop: '10xp;',
+	marginLeft: '5px',
+	width: '80%',
+	height: '30px',
+	background: '#3ba55d',
+};
+
+const AddFriendButton = () => {
+	const handleOpenAddFriend = () => {};
+
+	return (
+		<>
+			<CustomFriendButton
+				additionalStyles={additionalStyles}
+				label='Add Friend'
+				onClick={handleOpenAddFriend}
+			/>
+		</>
+	);
+};
+
+export default AddFriendButton;
