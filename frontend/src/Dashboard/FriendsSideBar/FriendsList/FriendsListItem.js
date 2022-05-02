f22ca5b051530d2370ccffe4a6eaa29ce14a0017
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-05-03 00:25:40 +0900

    create avatar

--- frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsListItem.js
+++ frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsListItem.js
@@ -1,7 +1,24 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import Avatar from '../../../shared/components/Avatar';
 
-const FriendsListItem = () => {
-	return <div>FriendsListItem</div>;
+const FriendsListItem = ({ id, username, isOnline }) => {
+	return (
+		<Button
+			style={{
+				width: '100%',
+				height: '42px',
+				marginTop: '10px',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'flex-start',
+				color: 'black',
+				position: 'relative',
+			}}
+		>
+			<Avatar username={username} />
+		</Button>
+	);
 };
 
 export default FriendsListItem;
